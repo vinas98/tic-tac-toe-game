b0063f92623ee7ffd984b496d70523fbd173511e
vinas anghan <50435783+vinas98@users.noreply.github.com>
2023-04-26 14:02:05 -0400

    Update Box.js

--- tictactoegame/src/Component/Box.js
+++ tictactoegame/src/Component/Box.js
@@ -1,15 +1,12 @@
 import React from 'react';
+import '../Component/Box.css';
 
+export const Box = ({ value, onClick }) => {
+  const style = value === 'x' ? 'box x' : 'box o';
 
-import "../Component/Box.css";
-
-
-export const Box = ({value , onClick}) => {
-
-const style = value === "x" ? "box x" : "box O" ;
-
-    return(
-        <button className={style} onClick={onClick} > {value} </button>
-
-    )
-}
+  return (
+    <button className={style} onClick={onClick}>
+      {value}
+    </button>
+  );
+};
